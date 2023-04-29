6bdd390b761b16d20833663085b18ec37c5128a2
monysary <sary.mony@gmail.com>
2023-04-28 22:51:29 -0700

    added some styling to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,28 @@
-import { useState } from "react"
-import axios from "axios"
+import Link from 'next/link'
+import { HiArrowRight, HiArrowLeft } from 'react-icons/hi'
 
 export default function Home() {
-  const [test, setTest] = useState('')
-
-  const testingAPI = async () => {
-    try {
-      const res = await axios.get('/api/hello')
-      setTest(res.data.name)
-    } catch (err) {
-      console.log(err);
-    }
-  }
 
   return (
-    <>
-      <h1>Homepage</h1>
-      <button onClick={testingAPI}>Check API</button>
-      <p>{test}</p>
-    </>
+    <div className="flex min-h-full flex-col justify-left items-center px-6 py-12 md:px-[200px] mt-[100px]">
+      <div className="font-bold">
+        <div className="text-[50px]">
+          Cook <span className="text-orange-600">smarter,</span> not harder. <br />
+        </div>
+        <div className="text-gray-600 text-[30px]">
+          Say goodbye to home meal planning stress with tasty recipes at your fingertips!
+        </div>
+      </div>
+      <div className="text-[30px] mt-[100px] flex">
+        <Link href='/pantry' className='flex items-center cursor-pointer'>
+          <HiArrowLeft />
+          <div className='ml-[20px]'>Pantry</div>
+        </Link>
+        <Link href='/kitchen' className='flex items-center cursor-pointer'>
+          <div className="ml-[150px]">Kitchen</div>
+          <HiArrowRight className='ml-[20px]' />
+        </Link>
+      </div>
+    </div>
   )
 }
