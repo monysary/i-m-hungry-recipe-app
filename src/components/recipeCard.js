4a8863f99cfe98a3e0939915764686b79ebf745a
monysary <sary.mony@gmail.com>
2023-05-04 16:58:29 -0700

    created a formstate for the search bar

---
+++ src/components/recipeCard.js
@@ -0,0 +1,5 @@
+function recipeCard() {
+    return (
+        <></>
+    )
+}
--- src/pages/kitchen.js
+++ src/pages/kitchen.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import authService from "@/utils/authService";
 import axios from "axios";
@@ -23,6 +23,17 @@ function Kitchen() {
         }
     }
 
+    const [searchState, setSearchState] = useState('')
+    const handleSearch = ({ target: { value } }) => {
+        setSearchState(value)
+    }
+
+    const searchSubmit = (event) => {
+        event.preventDefault()
+
+        console.log(searchState)
+    }
+
     return (
         <>
             <Head>
@@ -30,17 +41,22 @@ function Kitchen() {
             </Head>
             <div className="flex min-h-full flex-col justify-center md:px-[200px] px-6 py-12">
                 <div className="md:text-[30px] text-[16px] mb-[10px]">Time to cook!</div>
-                <form>
+                <form onSubmit={searchSubmit}>
                     <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                     <div className="relative">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                         </div>
-                        <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-teal-500 focus:border-teal-500 " placeholder="Search Ingredients..." required />
+                        <input
+                            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-teal-500 focus:border-teal-500 "
+                            placeholder="Search Ingredients..."
+                            type="search"
+                            value={searchState}
+                            onChange={handleSearch}
+                        />
                         <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-teal-500 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
                     </div>
                 </form>
-
             </div>
         </>
     )
