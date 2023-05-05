4251ddde22d3bbbc0e7308f644ddbb5c496070ca
Bryant Trinh <trinhbryant@gmail.com>
2023-05-05 13:28:57 -0700

    added my env file and creating an account works for me now

--- src/db/config/connections.js
+++ src/db/config/connections.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
