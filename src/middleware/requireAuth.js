3be3d29d3f40065a98ad7c450bd06b03fd9b007a
Darius Garcia <dariusgarcia888@gmail.com>
2023-05-05 13:34:44 -0700

    altered auth middleware function

--- src/middleware/requireAuth.js
+++ src/middleware/requireAuth.js
@@ -1,9 +1,22 @@
 const isAuthenticated = (req, res, next) => {
-  if (!localStorage.getItem('auth_token')) {
-    res.redirect('/login')
-  } else {
-    next()
+  let token = req.body.token || req.query.token || req.headers.authorization
+
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim()
   }
+
+  if (!token) {
+    return req
+  }
+
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: '2h' })
+    req.user = data
+  } catch {
+    console.log('Invalid token')
+  }
+
+  return req
 }
 
 module.exports = isAuthenticated
