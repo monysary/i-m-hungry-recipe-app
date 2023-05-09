91968acffc6d84db87c05cc6a5a9d12c5e0044ad
monysary <sary.mony@gmail.com>
2023-05-09 11:39:57 -0700

    moved save recipe button to recipeCard component so it only shows when a recipe is generated

--- src/components/recipeCard.js
+++ src/components/recipeCard.js
@@ -38,6 +38,9 @@ function RecipeCard() {
                     </div>
                 </dl>
             </div>
+            <div className="relative mb-12">
+                <button type="button" className="absolute right-0 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Save Recipe</button>
+            </div>
         </div>
     )
 }
--- src/pages/kitchen.js
+++ src/pages/kitchen.js
@@ -130,9 +130,6 @@ function Kitchen() {
                 </div>
                 <button type="button" className="text-white bg-cyan-500 hover:bg-cyan-600 focus:ring-4 focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Generate Recipe</button>
                 <RecipeCard />
-                <div className="relative mb-12">
-                    <button type="button" className="absolute right-0 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Save Recipe</button>
-                </div>
             </div >
         </>
     )
