f17cc052154e61e54a004689d26b3fb0c9a18b08
monysary <sary.mony@gmail.com>
2023-05-09 11:37:39 -0700

    added save button to kitchen page to save ai generated recipe

--- src/components/recipeCard.js
+++ src/components/recipeCard.js
@@ -18,8 +18,8 @@ function RecipeCard() {
                 <h3 className="text-base font-semibold leading-7 text-gray-900">Garlic Chicken with Apple and Potato Mash</h3>
                 <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">4 servings</p>
             </div>
-            <div className="mt-6 border-t border-gray-100">
-                <dl className="divide-y divide-gray-100">
+            <div className="mt-6 border-t border-gray-200">
+                <dl className="divide-y divide-gray-200">
                     <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                         <dt className="text-sm font-medium leading-6 text-gray-900">Ingredients</dt>
                         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{ingredients.join(', ')}</dd>
--- src/db/model/user.js
+++ src/db/model/user.js
@@ -3,86 +3,86 @@ const sequelize = require("../config/connections");
 const bcrypt = require("bcrypt");
 
 class User extends Model {
-	checkPassword(loginPassword) {
-		return bcrypt.compareSync(loginPassword, this.password);
-	}
+    checkPassword(loginPassword) {
+        return bcrypt.compareSync(loginPassword, this.password);
+    }
 }
 
 User.init(
-	{
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notNull: {
+                    msg: "Please enter a username",
+                },
+                notEmpty: {
+                    msg: "Please enter a username",
+                },
+            },
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                msg: "Email already in use!",
+            },
+            validate: {
+                notNull: {
+                    msg: "Please enter an email",
+                },
+                notEmpty: {
+                    msg: "Please enter an email",
+                },
+                isEmail: {
+                    msg: "Please enter a valid email address",
+                },
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "Please enter a password",
+                },
+                notEmpty: {
+                    msg: "Please enter a password",
+                },
+                len: {
+                    args: [8, 30],
+                    msg: "Password must be between 8 and 30 characters",
+                },
+            },
+        },
     },
-		username: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			unique: true,
-      validate: {
-        notNull: {
-          msg: "Please enter a username",
+    {
+        hooks: {
+            beforeCreate: async (newUser) => {
+                newUser.password = await bcrypt.hash(newUser.password, 10);
+                return newUser;
+            },
+            beforeUpdate: async (updateUser) => {
+                if (updateUser.password) {
+                    updateUser.password = await bcrypt.hash(updateUser.password, 10);
+                }
+                return updateUser;
+            },
         },
-        notEmpty: {
-          msg: "Please enter a username",
-        },
-      },
-		},
-		email: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			unique: {
-        msg: "Email already in use!",
-      },
-			validate: {
-        notNull: {
-          msg: "Please enter an email",
-        },
-        notEmpty: {
-          msg: "Please enter an email",
-        },
-				isEmail: {
-          msg: "Please enter a valid email address",
-        },
-			},
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-        notNull: {
-          msg: "Please enter a password",
-        },
-        notEmpty: {
-          msg: "Please enter a password",
-        },
-				len: {
-          args: [8,30],
-          msg: "Password must be between 8 and 30 characters",
-        },
-			},
-		},
-	},
-	{
-		hooks: {
-			beforeCreate: async (newUser) => {
-				newUser.password = await bcrypt.hash(newUser.password, 10);
-				return newUser;
-			},
-			beforeUpdate: async (updateUser) => {
-				if (updateUser.password) {
-					updateUser.password = await bcrypt.hash(updateUser.password, 10);
-				}
-				return updateUser;
-			},
-		},
-		sequelize,
-		timestamps: false,
-		freezeTableName: true,
-		underscored: true,
-		modelName: "hungry_user",
-	}
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "hungry_user",
+    }
 );
 
 module.exports = User;
