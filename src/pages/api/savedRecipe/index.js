5923bea8c99f1bed7b66e499b7711868c2e2f800
Bryant Trinh <trinhbryant@gmail.com>
2023-05-13 13:10:26 -0700

    updated savedRecipe model

--- src/pages/api/savedRecipe/index.js
+++ src/pages/api/savedRecipe/index.js
@@ -1,53 +1,49 @@
-const { SavedRecipe } = require('../../../db/model/index.js')
+const { SavedRecipe } = require("../../../db/model/index.js");
+import { isAuthenticated } from "../../../utils/authMiddleware";
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const savedRecipes = await SavedRecipe.findAll();
-      res.status(200).json(savedRecipes)
-    } catch (error) {
-      console.error(error)
-      res.status(400).json({ message: 'Failed to fetch saved recipes' })
-    }
-    // title, servings, ingredients, instructions
-  } else if (req.method === 'POST') {
-    const { title, servings, ingredients, instructions } = req.body
-    try {
-      const newSavedRecipe = await SavedRecipe.create({ name, ingredients, instructions })
-      res.status(200).json(newSavedRecipe)
-    } catch (error) {
-      console.error(error)
-      res.status(400).json({ message: 'Failed to save recipe' })
-    }
-  } else if (req.method === 'PUT') {
-    const { id } = req.query
-    const { title, servings, ingredients, instructions } = req.body;
-    try {
-      const updatedRecipe = await SavedRecipe.update(
-				{ title, servings, ingredients, instructions },
-				{ where: { id: id } }
-			);
-      if (!updatedRecipe) {
-        return res.status(404).json({ message: 'Recipe not found' })
-      }
-      res.status(200).json(`${id} has been updated`)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: 'Failed to update recipe' })
-    }
-  } else if (req.method === 'DELETE') {
-    const { id } = req.query
-    try {
-      const deletedRecipe = await SavedRecipe.destroy({ where: { id: id } });
-      if (!deletedRecipe) {
-        return res.status(404).json({ message: 'Recipe not found' })
-      }
-      res.status(200).json(`${id} has been deleted`)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: 'Failed to delete recipe' })
-    }
-  } else {
-    res.status(400).json({ message: 'Invalid request' })
-  }
+	if (req.method === "GET") {
+		isAuthenticated(req, res, async () => {
+			try {
+				const savedRecipes = await SavedRecipe.findAll();
+				res.status(200).json(savedRecipes);
+			} catch (error) {
+				console.error(error);
+				res.status(400).json({ message: "Failed to fetch saved recipes" });
+			}
+		});
+	} else if (req.method === "POST") {
+		isAuthenticated(req, res, async () => {
+			const { username, title, servings, ingredients, instructions } = req.body;
+			try {
+				const newSavedRecipe = await SavedRecipe.create({
+					username,
+					title,
+					servings,
+					ingredients,
+					instructions,
+				});
+				res.status(200).json(newSavedRecipe);
+			} catch (error) {
+				console.error(error);
+				res.status(400).json({ message: "Failed to save recipe" });
+			}
+		});
+	} else if (req.method === "DELETE") {
+		isAuthenticated(req, res, async () => {
+			const { id } = req.query;
+			try {
+				const deletedRecipe = await SavedRecipe.destroy({ where: { id: id } });
+				if (!deletedRecipe) {
+					return res.status(404).json({ message: "Recipe not found" });
+				}
+				res
+					.status(200)
+					.json({ message: `Recipe with id ${id} has been deleted` });
+			} catch (error) {
+				console.error(error);
+				res.status(400).json({ message: "Failed to delete recipe" });
+			}
+		});
+	}
 }
--- src/pages/kitchen.js
+++ src/pages/kitchen.js
@@ -98,99 +98,113 @@ function Kitchen() {
     }
 
     return (
-        <>
-            <Head>
-                <title>Chefing it up!</title>
-            </Head>
-            <div className='min-h-full md:px-[200px] px-6 py-12'>
-                <div className='md:text-[30px] text-[16px] mb-[10px] text-black'>
-                    Select Ingredients
-                </div>
-                <form className='sm:flex items-end mb-[20px]' onSubmit={addIngredient}>
-                    <div>
-                        <label className='block text-sm font-medium leading-6 text-gray-900'>
-                            Category
-                        </label>
-                        <div className='sm:mt-2'>
-                            <select
-                                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-teal-500 focus:border-teal-500 sm:max-w-xs sm:text-sm sm:leading-6'
-                                name='category'
-                                onChange={handleRecipeForm}
-                            >
-                                <option value=''>- select category -</option>
-                                {categories.map((category) => {
-                                    return (
-                                        <option key={category} value={category}>
-                                            {category}
-                                        </option>
-                                    )
-                                })}
-                            </select>
-                        </div>
-                    </div>
+			<>
+				<Head>
+					<title>Chefing it up!</title>
+				</Head>
+				<div className='min-h-full md:px-[200px] px-6 py-12'>
+					<div className='md:text-[30px] text-[16px] mb-[10px] text-black'>
+						To get started, please add your ingredients!
+					</div>
+					<form
+						className='sm:flex items-end mb-[20px]'
+						onSubmit={addIngredient}
+					>
+						<div>
+							<label className='block text-sm font-medium leading-6 text-gray-900'>
+								Category
+							</label>
+							<div className='sm:mt-2'>
+								<select
+									className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-teal-500 focus:border-teal-500 sm:max-w-xs sm:text-sm sm:leading-6'
+									name='category'
+									onChange={handleRecipeForm}
+								>
+									<option value=''>- select category -</option>
+									{categories.map((category) => {
+										return (
+											<option key={category} value={category}>
+												{category}
+											</option>
+										);
+									})}
+								</select>
+							</div>
+						</div>
 
-                    <div className='sm:ml-[20px] mt-2 sm:mt-0'>
-                        <label className='block text-sm font-medium leading-6 text-gray-900'>
-                            Ingredient
-                        </label>
-                        <div className='sm:mt-2'>
-                            <select
-                                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-teal-500 focus:border-teal-500 sm:max-w-xs sm:text-sm sm:leading-6'
-                                name='ingredient'
-                                onChange={handleRecipeForm}
-                            >
-                                <option value=''>- select ingredient -</option>
-                                {pantryItems
-                                    .filter((item) => item.category === recipeForm.category)
-                                    .map((item) => {
-                                        return (
-                                            <option key={item.ingredient} value={item.ingredient}>
-                                                {item.ingredient}
-                                            </option>
-                                        )
-                                    })}
-                            </select>
-                        </div>
-                    </div>
+						<div className='sm:ml-[20px] mt-2 sm:mt-0'>
+							<label className='block text-sm font-medium leading-6 text-gray-900'>
+								Ingredient
+							</label>
+							<div className='sm:mt-2'>
+								<select
+									className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-teal-500 focus:border-teal-500 sm:max-w-xs sm:text-sm sm:leading-6'
+									name='ingredient'
+									onChange={handleRecipeForm}
+								>
+									<option value=''>- select ingredient -</option>
+									{pantryItems
+										.filter((item) => item.category === recipeForm.category)
+										.map((item) => {
+											return (
+												<option key={item.ingredient} value={item.ingredient}>
+													{item.ingredient}
+												</option>
+											);
+										})}
+								</select>
+							</div>
+						</div>
 
-                    <button
-                        type='submit'
-                        className='mt-2 sm:mt-0 sm:ml-[20px] text-white bg-teal-500 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2'
-                    >
-                        Add
-                    </button>
-                </form >
-                <div className="flex flex-wrap gap-[10px] mb-[20px] py-6 border-t border-b border-gray-200">
-                    {ingredientsArr.map((ingredient) => {
-                        return (
-                            <div key={ingredient} id={ingredient} className="relative text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5">
-                                {ingredient}
-                                <AiFillCloseCircle id={ingredient} className="absolute right-[-10px] top-[-10px] cursor-pointer text-[20px]"
-                                    onClick={removeIngredient}
-                                />
-                            </div>
-                        )
-                    })}
-                </div>
-                {
-                    isLoading
-                        ? <></>
-                        : <button
-                            className="text-white bg-cyan-500 hover:bg-cyan-600 focus:ring-4 focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-6"
-                            type="button"
-                            onClick={generateRecipe}
-                        >Generate Recipe</button>
-                }
-                <div className='flex justify-center'>
-                    {
-                        isLoading
-                            ? <CircleSpinner />
-                            : recipe !== null && <RecipeCard recipe={recipe} />
-                    }
-                </div>
-            </div >
-        </>
-    )
+						<button
+							type='submit'
+							className='mt-2 sm:mt-0 sm:ml-[20px] text-white bg-teal-500 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2'
+						>
+							Add
+						</button>
+					</form>
+					<label className='block text-sm font-medium leading-6 text-gray-900'>
+						Selected Ingredients:
+					</label>
+					<div className='flex flex-wrap gap-[10px] mb-[20px] py-6 border-t border-b border-gray-200'>
+						{ingredientsArr.map((ingredient) => {
+							return (
+								<div
+									key={ingredient}
+									id={ingredient}
+									className='relative text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5'
+								>
+									{ingredient}
+									<AiFillCloseCircle
+										id={ingredient}
+										className='absolute right-[-10px] top-[-10px] cursor-pointer text-[20px]'
+										onClick={removeIngredient}
+									/>
+								</div>
+							);
+						})}
+					</div>
+					{isLoading ? (
+						<></>
+					) : (
+						<button
+							className='text-white bg-cyan-500 hover:bg-cyan-600 focus:ring-4 focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-6'
+							type='button'
+							onClick={generateRecipe}
+						>
+							Generate Recipe
+						</button>
+					)}
+					<div className='flex justify-center'>
+						{isLoading ? (
+							<CircleSpinner />
+						) : (
+							recipe !== null && <RecipeCard recipe={recipe} />
+						)}
+					</div>
+				</div>
+			</>
+		);
 }
 
 export default Kitchen
