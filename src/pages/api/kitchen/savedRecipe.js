70aec384f27a2dc2ec2b5230b1b9a3013c8cb570
monysary <sary.mony@gmail.com>
2023-05-13 13:04:49 -0700

    commenting out savedRecipe endpoint

--- src/pages/api/kitchen/savedRecipe.js
+++ src/pages/api/kitchen/savedRecipe.js
@@ -1,47 +1,47 @@
-export default async function handler(req, res) {
-    addRecipe: async function (recipeData, id) {
-        try {
-            const user = await User.findByPk(id);
-            if (!user) {
-                throw new Error("User not found");
-            }
+// export default async function handler(req, res) {
+//     addRecipe: async function (recipeData, id) {
+//         try {
+//             const user = await User.findByPk(id);
+//             if (!user) {
+//                 throw new Error("User not found");
+//             }
 
-            const recipe = await SavedRecipe.create({
-                ...recipeData,
-                user_id: user.id,
-            });
+//             const recipe = await SavedRecipe.create({
+//                 ...recipeData,
+//                 user_id: user.id,
+//             });
 
-            return recipe;
-        } catch (err) {
-            console.log(err);
-            throw err;
-        }
-    },
-    updateRecipe: async function (recipeId, recipeData) {
-        try {
-            const recipe = await SavedRecipe.findByPk(recipeId);
-            if (!recipe) {
-                throw new Error("Recipe not found");
-            }
-            await recipe.update(recipeData);
-            return recipe;
-        } catch (err) {
-            console.log(err);
-            throw err;
-        }
-    },
-    deleteRecipe: async function (recipeId) {
-        try {
-            const recipe = await SavedRecipe.findByPk(recipeId);
-            if (!recipe) {
-                throw new Error("Recipe not found");
-            }
-            await recipe.destroy();
-            return recipe;
-        } catch (err) {
-            console.log(err);
-            throw err;
-        }
-    }
+//             return recipe;
+//         } catch (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//     },
+//     updateRecipe: async function (recipeId, recipeData) {
+//         try {
+//             const recipe = await SavedRecipe.findByPk(recipeId);
+//             if (!recipe) {
+//                 throw new Error("Recipe not found");
+//             }
+//             await recipe.update(recipeData);
+//             return recipe;
+//         } catch (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//     },
+//     deleteRecipe: async function (recipeId) {
+//         try {
+//             const recipe = await SavedRecipe.findByPk(recipeId);
+//             if (!recipe) {
+//                 throw new Error("Recipe not found");
+//             }
+//             await recipe.destroy();
+//             return recipe;
+//         } catch (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//     }
 
-}
+// }
