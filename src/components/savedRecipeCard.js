a1a439c3eb2eb485be4113301809d58f115607a9
monysary <sary.mony@gmail.com>
2023-05-17 10:52:44 -0700

    Updated mobile response of savedRecipe table

--- src/components/savedRecipeCard.js
+++ src/components/savedRecipeCard.js
@@ -18,17 +18,17 @@ export default function Example({ myRecipes }) {
   const checkbox = useRef()
   const [checked, setChecked] = useState(false)
   const [indeterminate, setIndeterminate] = useState(false)
-  const [selectedPeople, setSelectedPeople] = useState([])
+  const [selectedRecipe, setSelectedRecipe] = useState([])
 
   useLayoutEffect(() => {
-    const isIndeterminate = selectedPeople.length > 0 && selectedPeople.length < people.length
-    setChecked(selectedPeople.length === people.length)
+    const isIndeterminate = selectedRecipe.length > 0 && selectedRecipe.length < myRecipes?.length
+    setChecked(selectedRecipe.length === myRecipes?.length)
     setIndeterminate(isIndeterminate)
     checkbox.current.indeterminate = isIndeterminate
-  }, [selectedPeople])
+  }, [selectedRecipe])
 
   function toggleAll() {
-    setSelectedPeople(checked || indeterminate ? [] : people)
+    setSelectedRecipe(checked || indeterminate ? [] : myRecipes)
     setChecked(!checked && !indeterminate)
     setIndeterminate(false)
   }
@@ -36,10 +36,10 @@ export default function Example({ myRecipes }) {
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="mt-8 flow-root">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <div className="relative">
-              {selectedPeople.length > 0 && (
+              {selectedRecipe.length > 0 && (
                 <div className="absolute left-14 top-0 flex h-12 items-center space-x-3 bg-white sm:left-12">
                   <button
                     type="button"
@@ -49,10 +49,10 @@ export default function Example({ myRecipes }) {
                   </button>
                 </div>
               )}
-              <table className="min-w-full table-fixed divide-y divide-gray-300">
-                <thead>
+              <table className="w-full table-fixed divide-y divide-gray-300">
+                <thead className='w-full'>
                   <tr>
-                    <th scope="col" className="relative px-7 sm:w-12 sm:px-6">
+                    <th scope="col" className="relative px-7 w-12 sm:px-6">
                       <input
                         type="checkbox"
                         className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
@@ -61,60 +61,66 @@ export default function Example({ myRecipes }) {
                         onChange={toggleAll}
                       />
                     </th>
-                    <th scope="col" className={`min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900 
-                      ${selectedPeople.length > 0 && 'invisible'}
+                    <th scope="col" className={`w-auto py-3.5 pr-3 text-left text-sm font-semibold text-gray-900 
+                      ${selectedRecipe.length > 0 && 'invisible'}
                     `}>
                       Name
                     </th>
-                    <th scope="col" className="hidden lg:table-cell px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <th scope="col" className="hidden lg:table-cell px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:w-[10%]">
                       Servings
                     </th>
-                    <th scope="col" className="hidden lg:table-cell px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <th scope="col" className="hidden md:table-cell px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:w-[25%]">
                       Ingredients
                     </th>
-                    <th scope="col" className="hidden lg:table-cell px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <th scope="col" className="hidden lg:table-cell px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:w-[25%]">
                       Instructions
                     </th>
-                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-3">
+                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-3 md:w-[10%]">
                       <span className="sr-only">Edit</span>
                     </th>
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {people.map((person) => (
-                    <tr key={person.email} className={selectedPeople.includes(person) ? 'bg-gray-50' : undefined}>
-                      <td className="relative px-7 sm:w-12 sm:px-6">
-                        {selectedPeople.includes(person) && (
+                  {myRecipes?.map((recipe) => (
+                    <tr key={recipe.title} className={selectedRecipe.includes(recipe) ? 'bg-gray-50' : undefined}>
+                      <td className="relative px-7 w-12 sm:px-6">
+                        {selectedRecipe.includes(recipe) && (
                           <div className="absolute inset-y-0 left-0 w-0.5 bg-indigo-600" />
                         )}
                         <input
                           type="checkbox"
                           className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                          value={person.email}
-                          checked={selectedPeople.includes(person)}
+                          value={recipe.title}
+                          checked={selectedRecipe.includes(recipe)}
                           onChange={(e) =>
-                            setSelectedPeople(
+                            setSelectedRecipe(
                               e.target.checked
-                                ? [...selectedPeople, person]
-                                : selectedPeople.filter((p) => p !== person)
+                                ? [...selectedRecipe, recipe]
+                                : selectedRecipe.filter((p) => p !== recipe)
                             )
                           }
                         />
                       </td>
                       <td
                         className={classNames(
-                          'whitespace-nowrap py-4 pr-3 text-sm font-medium',
-                          selectedPeople.includes(person) ? 'text-indigo-600' : 'text-gray-900'
+                          'whitespace-nowrap w-auto py-4 pr-3 text-sm font-medium truncate',
+                          selectedRecipe.includes(recipe) ? 'text-indigo-600' : 'text-gray-900'
                         )}
                       >
-                        {person.name}
+                        {recipe.title}
                       </td>
-                      <td className="hidden lg:table-cell whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.title}</td>
-                      <td className="hidden lg:table-cell whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.email}</td>
-                      <td className="hidden lg:table-cell whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.role}</td>
-                      <td className="whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
+                      <td className="hidden lg:table-cell whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {recipe.servings}
+                      </td>
+                      <td className="hidden md:table-cell whitespace-nowrap px-3 py-4 text-sm text-gray-500 truncate">
+                        {JSON.parse(recipe.ingredients).map((ingredient) => ingredient.name).join(', ')}
+                      </td>
+                      <td className="hidden lg:table-cell whitespace-nowrap px-3 py-4 text-sm text-gray-500 truncate">
+                        {JSON.parse(recipe.instructions).map((instruction) => instruction).join(', ')}
+                      </td>
+                      <td className="whitespace-nowrap py-4 sm:pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
                         <a href="#" className="ml-[10px] sm:ml-0 text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2">
-                          Edit<span className="sr-only">, {person.name}</span>
+                          Edit<span className="sr-only">, {recipe.title}</span>
                         </a>
                       </td>
                     </tr>
