79c773bfe4f3bf1b807bbb523642050d6b37eb70
monysary <sary.mony@gmail.com>
2023-05-31 10:05:46 -0700

    added loading circle on user log in and sign up

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -39,7 +39,9 @@ function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
   const [showConfirmPass, setShowConfirmPass] = useState(false)
 
+  const [loading, setLoading] = useState(false)
   const userSignup = async () => {
+    setLoading(true)
     try {
       const response = await fetch('/api/user/signup', {
         method: 'POST',
@@ -64,6 +66,8 @@ function SignUp() {
     } catch (err) {
       console.log(err);
       alert('An error occurred during signup.');
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -187,7 +191,13 @@ function SignUp() {
                   : 'bg-gray-300 text-gray-500  '
                 }`}
             >
-              Sign Up
+              {loading
+                ? <svg className='animate-spin -ml-1 mr-3 h-5 w-5 text-white' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle className="opacity-25" cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+                  <path className='opacity-75' fill='currentColor' d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                : 'Sign Up'
+              }
             </button>
           </div>
         </form>
