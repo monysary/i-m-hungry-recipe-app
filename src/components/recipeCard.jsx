d0a5d362c4fae3fb82d1ec59d588ee2f4d820c4b
monysary <sary.mony@gmail.com>
2023-06-07 16:51:26 -0700

    added margin bottom to recipeCard component

--- src/components/recipeCard.jsx
+++ src/components/recipeCard.jsx
@@ -28,7 +28,7 @@ function RecipeCard({ recipe, isLoading }) {
     }, [isLoading])
 
     return (
-        <div>
+        <div className="mb-4">
             <div className="px-4 sm:px-0">
                 <h3 className="text-[25px] font-semibold leading-7 text-gray-900">{recipe?.title}</h3>
                 <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Servings: {recipe?.servings}</p>
