598510272865a24eed02cc0bc38f4079b4497425
Darius Garcia <dariusgarcia888@gmail.com>
2023-06-12 11:51:33 -0700

    fixed footer

--- src/pages/kitchen.jsx
+++ src/pages/kitchen.jsx
@@ -150,7 +150,7 @@ function Kitchen() {
       <Head>
         <title>Chefing it up!</title>
       </Head>
-      <div className='flex justify-center h-full pb-24 '>
+      <div className='flex justify-center min-h-[80vh] h-full pb-24 '>
         <div className='max-w-[1280px] w-full px-4 py-6'>
           <div className='min-h-full px-2 py-6'>
             <div className='relative flex items-start gap-1'>
