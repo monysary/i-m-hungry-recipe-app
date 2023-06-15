b39b1440909b5649bdd72dec6246e04ae3450c8e
Darius Garcia <dariusgarcia888@gmail.com>
2023-06-15 15:20:50 -0700

    .

--- src/db/config/connections.js
+++ src/db/config/connections.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const Sequelize = require("sequelize")
 
+const = maxPoolConnections = process.env.MAX_POOL_CONNECTIONS || 5
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
@@ -12,7 +13,7 @@ const sequelize = process.env.JAWSDB_URL
         dialect: "mysql",
         port: 3306,
         pool: {
-          max: process.env.MAX_POOL_CONNECTIONS, // maximum number of connections in the pool
+          max: maxPoolConnections,// maximum number of connections in the pool
           min: 0, // minimum number of connections in the pool
           acquire: 30000, // maximum time (in milliseconds) that a connection can be idle before being released
           idle: 10000, // maximum time (in milliseconds) that a connection can be idle before being closed
